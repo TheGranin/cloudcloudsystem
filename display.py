3d1d984898ae75a33aed7b663b3b3137c58416aa
Alexander Svendsen <alexandersvendsen@hotmail.com>
2013-04-14 17:14:28 +0200

    removed a race condition

--- display.py
+++ display.py
@@ -1,5 +1,5 @@
 from pygame.locals import *
-import pygame
+import pygame, copy
 import os
 #from cache import Cache 
 
@@ -13,13 +13,15 @@ class Display():
 	def caclucateAvg(self, cache):
 		cc = 0
 		im = 0
-		if len(cache.clDictHitMiss) < 100 or len(cache.ilDictHitMiss) < 100:
-			self.prosent = (len(cache.clDictHitMiss) + len(cache.ilDictHitMiss))/2
+		clDictHitMiss = deepcopy(cache.clDictHitMiss)
+		ilDictHitMiss = deepcopy(cache.ilDictHitMiss)
+		if len(clDictHitMiss) < 100 or len(ilDictHitMiss) < 100:
+			self.prosent = (len(clDictHitMiss) + len(ilDictHitMiss))/2
 			return cc,im
 		self.started = True
-		for i in cache.clDictHitMiss:
+		for i in clDictHitMiss:
 			im += i
-		for i in cache.ilDictHitMiss:
+		for i in ilDictHitMiss:
 			cc += i
 		return cc, im
 			
@@ -43,22 +45,26 @@ class Display():
 								self.running = False
 								os._exit(0)
 								return
-							
+						
 				pygame.draw.rect(self.screen, (3,3,3), (0, 0, self.screen.get_width(), self.screen.get_height()))
 				time_passed = clock.tick(10)
 				time_passed_seconds = time_passed / 1000.0
-				
-				if not self.started:
-					self.screen.blit(self.fontType.render("Gathering Data..."+str(self.prosent) + "%", 0, (255,0,0)), (10,90))
-				
-				self.avgIM, self.avgCC = self.caclucateAvg(cache)
-				self.screen.blit(self.fontType.render("Avg Image Hit: "+ str(self.avgIM) +"%", 0, (255,255,0)), (10,10))
-				self.screen.blit(self.fontType.render("Avg Cloud Hit: "+ str(self.avgCC) +"%", 0, (255,255,0)), (10,50))
-				resTime, maxTime, avgTime = timer.getValues()
-				
-				self.screen.blit(self.fontType2.render(str("CUR: %4dms  Max: %4dms"  % (resTime, maxTime)), 0, (255,255,0)), (10,190))
-				
-				self.screen.blit(self.fontType2.render(str("AVG(%d): %4dms"%(timer.getNumSamples(), avgTime)), 0,(255,255,0)), (10,210))
+				try:
+					if not self.started:
+						self.screen.blit(self.fontType.render("Gathering Data..."+str(self.prosent) + "%", 0, (255,0,0)), (10,90))
+					
+					self.avgIM, self.avgCC = self.caclucateAvg(cache)
+					self.screen.blit(self.fontType.render("Avg Image Hit: "+ str(self.avgIM) +"%", 0, (255,255,0)), (10,10))
+					self.screen.blit(self.fontType.render("Avg Cloud Hit: "+ str(self.avgCC) +"%", 0, (255,255,0)), (10,50))
+					timer2 = deepcopy(timer)
+					resTime, maxTime, avgTime = timer2.getValues()
+					
+					self.screen.blit(self.fontType2.render(str("CUR: %4dms  Max: %4dms"  % (resTime, maxTime)), 0, (255,255,0)), (10,190))
+					
+					self.screen.blit(self.fontType2.render(str("AVG(%d): %4dms"%(timer.getNumSamples(), avgTime)), 0,(255,255,0)), (10,210))
+				except Exception as e:
+					print "-------Race error, do not care--------"
+					print e
 				pygame.display.flip()
 		except Exception as e:
 			print "--------------------ERRROR ON PYGAME----------------------"
