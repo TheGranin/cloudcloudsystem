b117d6a7ba5ea62e14bd66727e31ca80b7fe48a6
Alexander Svendsen <alexander@svenhub.com>
2013-04-18 18:27:54 +0200

    Cleaned up the code and fixed and fixed the comments, also aded the server code to it's own folder

---
+++ server/cache.py
@@ -0,0 +1,110 @@
+import urllib2
+import Image
+import cStringIO
+import collections
+from config import *
+from limitedSizeDict import LimitedSizeDict
+from threadSafeTimer import ThreadSafeTimer
+import time
+
+
+timer3 = ThreadSafeTimer(99)
+
+
+class Cache():
+	"""
+	An in memory cache wrapper. Responsible to get the external data, only meant to be used for the Serverfrontend
+	"""
+	def __init__(self, imageCacheSize = 50, ccCacheSize = 50000):
+		self.ildict = LimitedSizeDict(imageCacheSize)
+		self.cldict = LimitedSizeDict(ccCacheSize)
+		self.ilDictHitMiss = collections.deque(maxlen=100)
+		self.clDictHitMiss = collections.deque(maxlen=100)
+
+	def calculateCloudiness(self, image):
+		"""
+		Calculates the cloudiness in a image in %, if something went wrong -1 is instead returned
+		Formula given by the assignment text
+		"""
+		startTime = time.time() * 1000
+		try:
+			
+			buff = cStringIO.StringIO()
+			buff.write(image)
+			buff.seek(0)
+			img = Image.open(buff)
+
+			pix = img.load()
+			
+			Nbw = 0;
+			N = 0; 
+			Nbright = 0; 
+			CC = 0.0
+			
+			for i in xrange(0,626):
+				for j in xrange(0,266):
+					r,g,b = pix[i, j]
+					if( ( r + b + g ) > 160 ): 
+						Nbright += 1
+					if ( b > ( (r + g) / 1.9 ) ):
+						Nbw += 1
+					else:
+						N+=1
+			if ( Nbright > ( 626*266 )*0.4 ):
+				CC = 100.0 - Nbw*100.0/(626*266)
+			else:
+				CC = 0.0 
+
+			return CC 
+			
+			
+		except IOError:
+			print "cannot convert", infile
+			return -1
+
+	def getImageAndCloudniess(self,date):
+		"""
+		Function that gets the image and returns a tuple of the image and cloudiness if the image exists, if not -1
+		"""
+		if date in self.cldict:
+			self.clDictHitMiss.append(1)
+			return None, self.cldict[date], date
+		
+
+		image = self.getImage(date)
+		if image == -1:
+			return -1
+		
+		CC = self.calculateCloudiness(image)
+		self.clDictHitMiss.append(0)
+		self.cldict[date] = CC
+		return (image,CC, date)
+		
+	def getImage(self, date):
+		"""
+		Returns the image from the image server
+		"""
+		if date in self.ildict:
+			self.ilDictHitMiss.append(1)
+			return self.ildict[date]
+		try:
+			startTime = time.time() * 1000
+			response = urllib2.urlopen(C2Server+ date.strftime("%Y/%m/%d/wcam0_%Y%m%d_%H%M.jpg") )
+			image = response.read()
+			self.ildict[date] = image
+			self.ilDictHitMiss.append(0)
+			timer3.time(startTime, time.time()*1000)
+			return image
+		except Exception as e:
+			print e
+			print "ERROR: Could not connect to the server or the image is not there"
+			return -1
+	
+
+
+
+if __name__ == '__main__':
+	cache = Cache()
+	date = datetime.datetime.strptime("/2003/03/22/0600", "/%Y/%m/%d/%H%M")
+	img = cache.getImage(date)
+	print cache.calculateCloudiness(img)
---
+++ server/limitedSizeDict.py
@@ -0,0 +1,52 @@
+from collections import OrderedDict
+
+
+#http://stackoverflow.com/questions/2437617/limiting-the-size-of-a-python-dictionary
+class LimitedSizeDict():
+	"""
+	A last inn first out list, with limited size given in the contrsucter
+	"""
+	
+	def __init__(self, size_limit, *args):
+		self.size_limit = size_limit
+		self.d = OrderedDict(*args)
+		self._check_size_limit()
+
+	def __setitem__(self, key, value):
+		self.d[key] =  value
+		self._check_size_limit()
+	def __getitem__(self, key):
+		value = self.d[key]
+		self.pop(key)
+		self[key] = value
+		return value
+
+	def __len__(self):
+		return len(self.d)
+	def __delitem__(self, k):
+		del self.d[k]
+	
+	def pop(self,key):
+		self.__delitem__(key)
+		
+	def __iter__(self):
+		return iter(self.d)
+
+	def _check_size_limit(self):
+		if self.size_limit is not None:
+			while len(self) > self.size_limit:
+				self.d.popitem(last=False)
+	def __str__(self):
+		return str(self.d)
+if __name__ == '__main__':
+	d = {'banana': 3, 'apple':4, 'pear': 1, 'orange': 2}
+	lm = LimitedSizeDict(4, d)
+	lm['jordon'] = 1
+	lm['satan'] = 1
+	print lm['banana']
+	lm.pop('jordon')
+	print lm
+	print 'satan' in lm
+	print lm
+
+
